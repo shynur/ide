023bb672f24343e81705801203849a583e66dc82
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-04 23:40:20 +0800

    Update launch-this-ide.bash

--- image/launch-this-ide.bash
+++ image/launch-this-ide.bash
@@ -1,1 +1,12 @@
+#!/bin/bash -l
+
+MY_MIHOMO_PORT=7890
+if my-mihomo.bash up &>/dev/null; then
+    echo "export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT" >>/etc/profile
+fi
+
+kimi --auto web --host --allow-remote-terminals --dangerous-bypass-auth --no-open &
+
 /usr/sbin/sshd -D
+
+wait
