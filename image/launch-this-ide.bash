557f4fd861196c7422aa225c596a277cdb2d7552
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-05 09:43:37 +0800

    Update launch-this-ide.bash

--- image/launch-this-ide.bash
+++ image/launch-this-ide.bash
@@ -1,6 +1,8 @@
 #!/bin/bash -l
 PS1=force_eval . ~/.bashrc
 
+chmod a-x -- "$0"
+
 MY_MIHOMO_PORT=7890
 if my-mihomo.bash up &>/dev/null; then
     echo "export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT" >>/etc/profile
