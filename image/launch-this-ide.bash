2547462433b5c6112ca018ec6db20d24156eadec
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-11 09:36:31 +0800

    Update launch-this-ide.bash

--- image/launch-this-ide.bash
+++ image/launch-this-ide.bash
@@ -8,8 +8,9 @@ if my-mihomo.bash up &>/dev/null; then
     echo "export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT" >>/etc/profile
 fi
 
-kimi --auto web --host --allow-remote-terminals --dangerous-bypass-auth --no-open &
+apt install -y sshpass gh {un,}zip zstd xz-utils jq rsync ripgrep file &>/dev/null &
 
+kimi --auto web --host --allow-remote-terminals --dangerous-bypass-auth --no-open &
 /usr/sbin/sshd -D
 
 wait
