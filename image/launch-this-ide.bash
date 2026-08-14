0213f2fea3bf0601d022a67dadd399de5da32485
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-14 23:23:15 +0800

    Update launch-this-ide.bash

--- image/launch-this-ide.bash
+++ image/launch-this-ide.bash
@@ -6,6 +6,7 @@ chmod a-x -- "$0"
 MY_MIHOMO_PORT=7890
 if my-mihomo.bash up &>/dev/null; then
     echo "export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT" >>/etc/profile
+    MY_MIHOMO_UP=1
 fi
 
 apt install -y sshpass gh {un,}zip zstd xz-utils jq rsync ripgrep file git-lfs xxd &>/dev/null &
@@ -19,7 +20,13 @@ while IFS= read -r url; do
     printf 'url=%s\n\n' "$url" | git credential approve
 done <~/.git-credentials
 
-kimi --auto web --host --allow-remote-terminals --dangerous-bypass-auth --no-open &
+(
+    if [ $MY_MIHOMO_UP ]; then
+        export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT
+    fi
+    kimi --auto web --host --allow-remote-terminals --dangerous-bypass-auth --no-open &
+)
+
 /usr/sbin/sshd -D
 
 wait
