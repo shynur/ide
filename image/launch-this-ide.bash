0228c31c6c1c190ca5fd4cf845d0b700b9a8daf3
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-20 10:24:51 +0800

    Update launch-this-ide.bash

--- image/launch-this-ide.bash
+++ image/launch-this-ide.bash
@@ -5,8 +5,12 @@ chmod a-x -- "$0"
 
 MY_MIHOMO_PORT=7890
 if my-mihomo.bash up &>/dev/null; then
-    echo "export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT" >>/etc/profile
     MY_MIHOMO_UP=1
+    MY_MIHOMO_HTTP_PROXY=http://127.0.0.1:$MY_MIHOMO_PORT MY_MIHOMO_HTTPS_PROXY=http://127.0.0.1:$MY_MIHOMO_PORT MY_MIHOMO_ALL_PROXY=socks5h://127.0.0.1:$MY_MIHOMO_PORT
+    {
+        echo "export http_proxy=$MY_MIHOMO_HTTP_PROXY https_proxy=$MY_MIHOMO_HTTPS_PROXY all_proxy=$MY_MIHOMO_ALL_PROXY"
+        echo 'export HTTP_PROXY=$http_proxy HTTPS_PROXY=$https_proxy ALL_PROXY=$all_proxy'
+    } >>/etc/profile
 fi
 
 apt install -y sshpass gh {un,}zip zstd xz-utils jq rsync ripgrep file git-lfs xxd &>/dev/null &
@@ -22,14 +26,16 @@ done <~/.git-credentials
 
 (
     if [ $MY_MIHOMO_UP ]; then
-        export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT
+        export http_proxy=$MY_MIHOMO_HTTP_PROXY https_proxy=$MY_MIHOMO_HTTPS_PROXY all_proxy=$MY_MIHOMO_ALL_PROXY
+        export HTTP_PROXY=$http_proxy           HTTPS_PROXY=$https_proxy           ALL_PROXY=$all_proxy
     fi
     kimi --auto web --host --allow-remote-terminals --dangerous-bypass-auth --no-open --port 58627 &
 )
 
 (
     if [ $MY_MIHOMO_UP ]; then
-        export http_proxy=http://127.0.0.1:$MY_MIHOMO_PORT https_proxy=http://127.0.0.1:$MY_MIHOMO_PORT all_proxy=socks5h://127.0.0.1:$MY_MIHOMO_PORT
+        export http_proxy=$MY_MIHOMO_HTTP_PROXY https_proxy=$MY_MIHOMO_HTTPS_PROXY all_proxy=$MY_MIHOMO_ALL_PROXY
+        export HTTP_PROXY=$http_proxy           HTTPS_PROXY=$https_proxy           ALL_PROXY=$all_proxy
     fi
     code --locale zh-CN serve-web --host 0.0.0.0 --without-connection-token --accept-server-license-terms --disable-telemetry --port 8000 &
 )
