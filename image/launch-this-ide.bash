3b6f2a03afaeff5c90ad746842cd65df2143e73d
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-22 00:25:40 +0800

    Update launch-this-ide.bash

--- image/launch-this-ide.bash
+++ image/launch-this-ide.bash
@@ -19,7 +19,7 @@ if my-mihomo.bash up &>/dev/null; then
     } >>/etc/profile
 fi
 
-apt install -y sshpass gh {un,}zip zstd xz-utils jq rsync ripgrep file git-lfs xxd &>/dev/null &
+apt install -y sshpass gh {un,}zip zstd xz-utils jq rsync ripgrep file git-lfs xxd >/dev/null &
 
 git config --global credential.helper "cache --timeout=$[2**31-1]"
 echo >>~/.git-credentials
